10e7235eb1594f89ce7dad9c94b139ceae98ca2d
Ashray Chowdhry <ashraychowdhry@gmail.com>
2021-03-05 16:22:50 -0500

    React project initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import react from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <>
+      <h2>Hello</h2>
+    </>
+  );
+}
+
+export default App;
